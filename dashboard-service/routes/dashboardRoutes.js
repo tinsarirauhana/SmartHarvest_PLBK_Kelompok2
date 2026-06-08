248e75c98557b6a3eb59ac1aa26d1e12738de8cb
tinsarirauhana <tinsari.r@mhs.usk.ac.id>
2026-06-08 14:51:10 +0700

    delete some services

--- dashboard-service/routes/dashboardRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { protect, adminOnly } = require('../config/auth');
-const { getDashboardStats, getHarvestTrend } = require('../controllers/dashboardController');
-
-router.get('/stats', protect, adminOnly, getDashboardStats);
-router.get('/harvest-trend', protect, adminOnly, getHarvestTrend);
-
-module.exports = router;
